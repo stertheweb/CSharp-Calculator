b30bf66e8a95a1f2261818a6e44e1dd1190a0f19
Malik Webster <mwebster30@gmail.com>
2017-04-10 13:58:02 -0500

    Simple C# Calculator Code
    Program C# file containing code.

---
+++ Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+
+namespace SimpleCalculator
+{
+	class MainClass
+	{
+		// Main Method
+		public static void Main(string[] args)
+		{
+			// Declarations
+			string x = "0";
+			string y = "0";
+			string operation = "A";
+			double xVal = 0.0;
+			double yVal = 0.0;
+			double z = 0.0;
+
+			// Greeting
+			Console.WriteLine("Welcome to my simple calculator.");
+			Console.WriteLine("Please enter 2 values and select an operation.");
+			Console.WriteLine("");
+
+			// Read in values
+			Console.WriteLine("Value 1: ");
+			x = Console.ReadLine();
+			bool xRes = double.TryParse(x, out xVal);
+			while (xRes == false)
+			{
+				Console.WriteLine("String is not a number. Try again!");
+				Console.WriteLine("Value 1: ");
+				x = Console.ReadLine();
+				xRes = double.TryParse(x, out xVal);
+			}
+			Console.WriteLine("Value 2: ");
+			y = Console.ReadLine();
+			bool yRes = double.TryParse(y, out yVal);
+			while (yRes == false)
+			{
+				Console.WriteLine("String is not a number. Try again!");
+				Console.WriteLine("Value 2: ");
+				y = Console.ReadLine();
+				yRes = double.TryParse(y, out yVal);
+			}
+
+			// Select an operation
+			Console.WriteLine("Select an Operation:");
+			Console.WriteLine("Press A for Addition");
+			Console.WriteLine("Press S for Subtraction");
+			Console.WriteLine("Press M for Multiplication");
+			Console.WriteLine("Press D for Division");
+			operation = Console.ReadLine();
+
+			// Sending selected operation to method
+			switch (operation)
+			{
+				case "A":
+				case "a":
+					z = Add(xVal, yVal);
+					break;
+				case "D":
+				case "d":
+					z = Divide(xVal, yVal);
+					break;
+				case "S":
+				case "s":
+					z = Subtract(xVal, yVal);
+					break;
+				case "M":
+				case "m":
+					z = Multiply(xVal, yVal);
+					break;
+				default:
+					z = Add(xVal, yVal);
+
+					break;
+			}
+
+			Console.WriteLine("Result: " + z);
+
+		}
+
+		// Add Method
+		static double Add(double value1, double value2)
+		{
+			double sum = 0.0;
+			sum = value1 + value2;
+			return sum;
+		}
+
+		// Subtract Method
+		static double Subtract(double value1, double value2)
+		{
+			double answer = 0.0;
+			answer = value1 - value2;
+			return answer;
+		}
+
+		// Multiply Method
+		static double Multiply(double value1, double value2)
+		{
+			double product = 0.0;
+			product = value1 * value2;
+			return product;
+		}
+
+		// Divide Method
+		static double Divide(double value1, double value2)
+		{
+			double result = 0.0;
+			result = value1 / value2;
+			return result;
+		}
+
+	}
+}
